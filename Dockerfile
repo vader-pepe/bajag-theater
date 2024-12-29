30fd83e3c485596a38af544c2f2d04cf21f48c1b
vader-pepe <vader-pepe@email.com>
2024-12-29 10:12:12 +0700

    feat: add bashscript

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Use a lightweight base image
+FROM python:3.11-alpine
+
+# Set environment variables for non-interactive builds
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Install required packages
+RUN apk add --no-cache \
+    ffmpeg \
+    curl \
+    bash
+
+# Install yt-dlp via pip
+RUN pip install --no-cache-dir yt-dlp
+
+# Create a directory for downloads
+WORKDIR /downloads
+
+# copy script from host
+COPY script.sh .
+RUN ls
+RUN chmod +x script.sh
+
+# Volume for persisting downloaded files
+VOLUME ["/downloads"]
+
+# Default command to show help message
+CMD ["bash", "script.sh"]
---
+++ script.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+
+# Check if the COOKIES environment variable is set and not empty
+if [ -n "$COOKIES" ]; then
+  cookies_file="$COOKIES"
+else
+  # If COOKIES is not set, check for the command-line argument
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <cookies_file> or set the COOKIES environment variable."
+    exit 1
+  fi
+  cookies_file="$1"
+fi
+
+# Check if the specified file exists
+if [ ! -f "$cookies_file" ]; then
+  echo "Error: The file '$cookies_file' does not exist."
+  exit 1
+fi
+
+URL="https://www.youtube.com/@JKT48TV/live"
+python3 yt-dlp --cookies $cookies_file $URL -o output -f "bestvideo[height=1080][fps=60][vbr=20000]+bestaudio[acodec=aac][abr=140]"
+current_date=$(date +"%Y-%m-%d_%H-%M-%S")
+mv output "${current_date}"
