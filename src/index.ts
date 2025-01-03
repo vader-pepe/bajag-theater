c97fda9ef305e32e1d8b1bebb2c0d4a2e6b1796b
vader-pepe <vader-pepe@email.com>
2025-01-03 11:38:55 +0700

    feat: re-stream done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import cron from 'node-cron';
-import { exec } from "child_process";
+import { exec } from "node:child_process";
+import cron from "node-cron";
+
 import { env } from "@/common/utils/envConfig";
 import { app, logger } from "@/server";
 
@@ -17,28 +18,33 @@ const onCloseSignal = () => {
   setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
 };
 
+let isDownloading = false;
 // Schedule job from 18:45 to 19:00 Monday to Friday
-cron.schedule('45-59 18 * * 1-5', () => {
-  exec('sh script.sh',
-    (error, stdout, stderr) => {
+cron.schedule("45-59 18 * * 1-5", () => {
+  if (!isDownloading) {
+    exec("sh script.sh", (error, stdout, stderr) => {
       console.log(stdout);
-      console.log(stderr);
+      isDownloading = true;
       if (error !== null) {
+        // const notif = "The channel is not currently live";
+        // const err = `${error}`;
         console.log(`exec error: ${error}`);
       }
     });
+  }
 });
 
 // Schedule job from 14:00 to 19:00 Saturday and Sunday
-cron.schedule('0-59 14-19 * * 6,7', () => {
-  exec('sh script.sh',
-    (error, stdout, stderr) => {
+cron.schedule("0-59 14-19 * * 6,7", () => {
+  if (!isDownloading) {
+    exec("sh script.sh", (error, stdout, stderr) => {
       console.log(stdout);
-      console.log(stderr);
+      isDownloading = true;
       if (error !== null) {
         console.log(`exec error: ${error}`);
       }
     });
+  }
 });
 
 process.on("SIGINT", onCloseSignal);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
+import path from "node:path";
 import cors from "cors";
 import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
-import { statSync, createReadStream, existsSync } from "fs"
-import path from "path";
-import ffmpeg from "fluent-ffmpeg"
 
-import { openAPIRouter } from "@/api-docs/openAPIRouter";
 import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
 // import { userRouter } from "@/api/user/userRouter";
 import errorHandler from "@/common/middleware/errorHandler";
@@ -24,9 +21,11 @@ app.set("trust proxy", true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
-app.use(helmet({
-  contentSecurityPolicy: false
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
 app.use(rateLimiter);
 
 // Request logging
@@ -35,41 +34,14 @@ app.use(requestLogger);
 // Routes
 app.use("/health-check", healthCheckRouter);
 app.use("/ffmpeg", express.static(path.resolve("node_modules/@ffmpeg")));
-// app.use("/users", userRouter);
-app.get("/", function(_req, res) {
-  res.sendFile(__dirname + "/index.html");
-})
+app.get("/", (_req, res) => {
+  res.sendFile(`${__dirname}/view.html`);
+});
 
-// TODO: not yet fixed!
-app.get("/livestream", function(req, res) {
-  const videoPath = path.resolve(__dirname, "../output.part");
-  if (!existsSync(videoPath)) {
-    return res.status(404).send('File not found');
-  }
-
-  let range = req.headers.range;
-  if (!range) {
-    range = "bytes=0-";
-  }
-
-  const videoSize = statSync(videoPath).size;
-  const CHUNK_SIZE = 100 ** 6;
-  const start = Number(range.replace(/\D/g, ""))
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);;
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mpeg",
-  };
-  res.writeHead(206, headers);
-  const videoStream = createReadStream(videoPath, { start, end });
-  return videoStream.pipe(res);
-})
+app.use("/livestream", express.static(path.resolve("video")));
 
 // Swagger UI
-app.use(openAPIRouter);
+// app.use(openAPIRouter);
 
 // Error handlers
 app.use(errorHandler());
