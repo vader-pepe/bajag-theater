07d309bc3861fb0623502dfe9c8283de289c54c2
vader-pepe <vader-pepe@email.com>
2025-03-10 21:45:55 +0700

    feat: add more debugger for nvenc

--- src/api/livestream/livesreamRouter.ts
+++ src/api/livestream/livesreamRouter.ts
@@ -101,10 +101,9 @@ livesreamRouter.get("/video.mp4", async (_req, res) => {
           "-c:v h264_nvenc", // Use NVENC encoder.
           "-movflags frag_keyframe+empty_moov",
         ])
-        .on("progress", (prg) => {
-          logger.info("nvidia nvenc ran!");
-        })
         .outputFormat("mp4")
+        .on("start", (cmd) => logger.info(`command: ${cmd}`))
+        .on("progress", (prg) => logger.info(`frames: ${prg.frames}`))
         .on("error", (err) => logger.error(err))
         .pipe(res, { end: true });
     }
